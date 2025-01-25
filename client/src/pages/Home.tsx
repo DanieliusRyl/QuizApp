f03687d6ed4acd92d7816609542c2b4770173815
Danielius <danieliusryliskis.dev@gmail.com>
2025-01-25 15:04:11 +0200

    add Home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__content">
+        <h1 className="home__title">Welcome to the Quiz App</h1>
+        <div className="home__links">
+          <Link to="/quiz" className="home__link">
+            Quiz
+          </Link>
+          <Link to="/leaderboard" className="home__link">
+            High Scores
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
