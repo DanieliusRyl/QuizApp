500638a15dd2911415ff281ceaab393a1eee3bba
Danielius <danieliusryliskis.dev@gmail.com>
2025-01-25 14:50:50 +0200

    add AnswerSubmission

---
+++ server/Models/DTOs/AnswerSubmission.cs
@@ -0,0 +1,5 @@
+public class AnswerSubmission
+{
+    public int QuestionId { get; set; }
+    public List<string> Answers { get; set; } = new();
+}
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2892307a88a3bdaf92b5cbf70a8fb1f8896adfd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6285e650a287e79ce66951391332f592ee372f3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
