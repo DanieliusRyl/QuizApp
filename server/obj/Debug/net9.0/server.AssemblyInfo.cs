06aac9f2005350d10866200727f43e653d5863b8
Danielius <danieliusryliskis.dev@gmail.com>
2025-01-25 14:59:05 +0200

    init react/ts

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb8968c9bfcf99df0d8404175b395f239d923020")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b965da7352c16fa28a16239ef0102bea14d3d94e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
